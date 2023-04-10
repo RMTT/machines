55392811233368709d2f0e1bfb8c151eca27b129
RMT <d.rong@outlook.com>
2023-04-10 22:15:56 +0800

    neovim tweaks

--- modules/developments.nix
+++ modules/developments.nix
@@ -10,5 +10,6 @@
     nix-direnv
     cmake
     gradle
+    gnumake
   ];
 }
--- users/modules/config/nvim/init.lua
+++ users/modules/config/nvim/init.lua
@@ -8,7 +8,7 @@ vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.shiftround = true
-vim.o.autowrite = true
+vim.o.autowriteall = true
 vim.o.report = 0
 vim.o.relativenumber = true
 vim.o.number = true
@@ -32,12 +32,13 @@ utility.mkdir(vim.o.directory)
 vim.o.encoding = 'utf-8'
 
 vim.o.completeopt = "menu,menuone,noselect"
----- end ----
 
----- global ----
-
-vim.g.python3_host_prog = "~/.pyenv/versions/nvim/bin/python"
-
+-- auto-reload files when modified externally
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
 ---- end ----
 
 ---- load plugins ----
