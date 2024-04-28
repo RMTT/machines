87824558bf2b55afd41e51a0a3f4ea17e565684d
RMT <d.rong@outlook.com>
2024-04-28 21:46:15 +0800

    update

--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -25,15 +25,16 @@ in with lib; {
       obsidian
       virt-manager
       zoom-us
-			openconnect
+      openconnect
     ];
 
     # fonts
     fonts.fontDir.enable = true;
     fonts.enableDefaultPackages = true;
     fonts.packages = with pkgs; [
-			wqy_zenhei
+      wqy_zenhei
       noto-fonts
+      noto-fonts-cjk
       sarasa-gothic
       joypixels
       noto-fonts-emoji
@@ -50,7 +51,7 @@ in with lib; {
           "Sarasa Mono Slab K"
         ];
         sansSerif =
-          [ "Sarasa UI SC" "Sarasa UI TC" "Sarasa UI J" "Sarasa UI K" ];
+          [ "Sarasa UI SC" "Sarasa UI TC" "Sarasa UI J" "Sarasa UI K" "Noto Sans CJK SC" ];
         monospace =
           [ "Sarasa Mono SC" "Sarasa Mono TC" "Sarasa Mono J" "Sarasa Mono K" ];
       };
@@ -79,7 +80,7 @@ in with lib; {
     hardware.logitech.wireless.enable = true;
     hardware.logitech.wireless.enableGraphical = true;
 
-		services.input-remapper.enable = true;
-		services.fwupd.enable = true;
+    services.input-remapper.enable = true;
+    services.fwupd.enable = true;
   };
 }
