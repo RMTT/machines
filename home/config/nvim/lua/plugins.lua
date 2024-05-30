0ae2d9427059055ee25c6b33c184a59ba2a7799e
RMT <d.rong@outlook.com>
2024-05-30 17:11:55 +0800

    update neovim

--- home/config/nvim/lua/plugins.lua
+++ home/config/nvim/lua/plugins.lua
@@ -108,8 +108,6 @@ cmp.setup {
         end,
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),  -- Down
         ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
