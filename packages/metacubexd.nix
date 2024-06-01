67144fd6ebab18fd7c83612bc0a399b1b22be033
RMT <d.rong@outlook.com>
2024-06-01 17:08:15 +0800

    update metacubexd

--- packages/metacubexd.nix
+++ packages/metacubexd.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, ... }:
 let
-  version = "1.136.1";
+  version = "1.138.1";
   src = fetchurl {
     name = "metacubexd";
     url =
       "https://github.com/MetaCubeX/metacubexd/releases/download/v${version}/compressed-dist.tgz";
-    sha256 = "sha256-5hYanG3tUSfsHdVBo/roNoZPcbBpBeals/4wHvYWhUs=";
+    sha256 = "sha256-l2mcb2optvgtfzma/Ix63Y8BxXC6CsGoyIIMoOvD074=";
   };
 in stdenv.mkDerivation {
   pname = "metacubexd";
