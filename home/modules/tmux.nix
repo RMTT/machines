8bf5666996777b50650b26f2bfe1df34447679e1
RMT <d.rong@outlook.com>
2024-06-12 17:29:08 +0800

    replace nord-tmux by catppuccin

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -1,50 +1,48 @@
 { pkgs, lib, ... }:
-let
-  nord-tmux = pkgs.tmuxPlugins.mkTmuxPlugin {
-    pluginName = "nord";
-    version = "0.3";
-    src = fetchGit {
-      url = "https://github.com/nordtheme/tmux";
-      ref = "develop";
-      rev = "f7b6da07ab55fe32ee5f7d62da56d8e5ac691a92";
-    };
-  };
-in
 {
   programs.tmux = {
     enable = true;
     mouse = true;
     keyMode = "vi";
     clock24 = true;
+    escapeTime = 0;
     extraConfig = ''
-                        			set -s default-terminal 'screen-256color'
+      set -s default-terminal 'screen-256color'
 
-            									unbind-key C-b
-            									set-option -g prefix C-x
-            									bind-key C-x send-prefix
+      unbind-key C-b
+      set-option -g prefix C-x
+      bind-key C-x send-prefix
 
-                        			bind -T copy-mode-vi v send -X begin-selection
-                              bind -T copy-mode-vi C-v send -X rectangle-toggle
-                              bind -T copy-mode-vi y send -X copy-selection-and-cancel
-                              bind -T copy-mode-vi Escape send -X cancel
-                              bind y run -b "\"\$TMUX_PROGRAM\" \''${TMUX_SOCKET:+-S \"\$TMUX_SOCKET\"} save-buffer - | wl-copy"
+      bind -T copy-mode-vi v send -X begin-selection
+      bind -T copy-mode-vi C-v send -X rectangle-toggle
+      bind -T copy-mode-vi y send -X copy-selection-and-cancel
+      bind -T copy-mode-vi Escape send -X cancel
+      bind y run -b "\"\$TMUX_PROGRAM\" \''${TMUX_SOCKET:+-S \"\$TMUX_SOCKET\"} save-buffer - | wl-copy"
 
-                        			bind -n M-H select-pane -L
-                              bind -n M-L select-pane -R
-                              bind -n M-K select-pane -U
-      												bind -n M-J select-pane -D
+      bind -n M-H select-pane -L
+      bind -n M-L select-pane -R
+      bind -n M-K select-pane -U
+      bind -n M-J select-pane -D
 
-                        			bind -n M-P previous-window
-                              bind -n M-N next-window
-
-      												set-option -g status-interval 5
-      												set-option -g automatic-rename on
-      												set-option -g automatic-rename-format '#{b:pane_current_path}'
-
-                        			set -sg escape-time 0
-
-                  						set -g @plugin "nordtheme/tmux"
+      bind -n M-P previous-window
+      bind -n M-N next-window
     '';
-    plugins = [ nord-tmux ];
+    plugins = with pkgs.tmuxPlugins; [{
+      plugin = catppuccin;
+      extraConfig = ''
+        set -g @catppuccin_window_left_separator "█"
+        set -g @catppuccin_window_right_separator "█ "
+        set -g @catppuccin_window_number_position "right"
+        set -g @catppuccin_window_middle_separator "  █"
+        set -g @catppuccin_window_default_fill "number"
+        set -g @catppuccin_window_current_fill "number"
+        set -g @catppuccin_window_current_text "#{b:pane_current_path}"
+        set -g @catppuccin_status_modules_right "application session date_time"
+        set -g @catppuccin_status_left_separator  ""
+        set -g @catppuccin_status_right_separator " "
+        set -g @catppuccin_status_fill "all"
+        set -g @catppuccin_status_connect_separator "yes"
+          				'';
+    }];
   };
 }
