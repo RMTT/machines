1bbdf9fb65f174e70cb05246f824c2fcda47b1b9
RMT <d.rong@outlook.com>
2024-06-13 14:49:06 +0800

    update tmux

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -30,18 +30,7 @@
     plugins = with pkgs.tmuxPlugins; [{
       plugin = catppuccin;
       extraConfig = ''
-        set -g @catppuccin_window_left_separator "█"
-        set -g @catppuccin_window_right_separator "█ "
-        set -g @catppuccin_window_number_position "right"
-        set -g @catppuccin_window_middle_separator "  █"
-        set -g @catppuccin_window_default_fill "number"
-        set -g @catppuccin_window_current_fill "number"
-        set -g @catppuccin_window_current_text "#{b:pane_current_path}"
         set -g @catppuccin_status_modules_right "application session date_time"
-        set -g @catppuccin_status_left_separator  ""
-        set -g @catppuccin_status_right_separator " "
-        set -g @catppuccin_status_fill "all"
-        set -g @catppuccin_status_connect_separator "yes"
           				'';
     }];
   };
