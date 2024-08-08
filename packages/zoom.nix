43d607998b1206290f3629cd1f40b004cd7ddacc
RMT <d.rong@outlook.com>
2024-08-08 14:41:04 +0800

    update zoom

--- packages/zoom.nix
+++ packages/zoom.nix
@@ -48,23 +48,12 @@ let
   # and often with different versions.  We write them on three lines
   # like this (rather than using {}) so that the updater script can
   # find where to edit them.
-  versions.aarch64-darwin = "6.1.1.36333";
-  versions.x86_64-darwin = "6.1.1.36333";
-  versions.x86_64-linux = "6.1.1.443";
+  versions.x86_64-linux = "6.1.6.1013";
 
   srcs = {
-    aarch64-darwin = fetchurl {
-      url = "https://zoom.us/client/${versions.aarch64-darwin}/zoomusInstallerFull.pkg?archType=arm64";
-      name = "zoomusInstallerFull.pkg";
-      hash = "sha256-CBBJAa7hnz0I2ctEn7DMdzeXEs4x+aEmEr+L42ddqXE=";
-    };
-    x86_64-darwin = fetchurl {
-      url = "https://zoom.us/client/${versions.x86_64-darwin}/zoomusInstallerFull.pkg";
-      hash = "sha256-CHtyL/BdyBVCQOGWjP0H/5GJiq67hPNQxELlvzzUuts=";
-    };
     x86_64-linux = fetchurl {
-      url = "https://zoom.us/client/${versions.x86_64-linux}/zoom_x86_64.pkg.tar.xz";
-      hash = "sha256-2FOAZ3MKusouuWvhxFEcqX+2e+PCF4N5zaz7mc9Mnq4=";
+      url = "https://cdn.zoom.us/prod/${versions.x86_64-linux}/zoom_x86_64.pkg.tar.xz";
+      hash = "sha256-mvCJft0suOxnwTkWWuH9OYKHwTMWx61ct10P5Q/EVBM=";
     };
   };
 
@@ -181,7 +170,6 @@ stdenv.mkDerivation rec {
       --chdir "$out/opt/zoom" \
       --unset QML2_IMPORT_PATH \
       --unset QT_PLUGIN_PATH \
-      --unset QT_SCREEN_SCALE_FACTORS \
       --prefix PATH : ${lib.makeBinPath [ coreutils glib.dev pciutils procps util-linux ]} \
       --prefix LD_LIBRARY_PATH ":" ${libs}
 
