9e9eafa77cd96eed90f68f2d491bae8ad3430bbe
RMT <d.rong@outlook.com>
2024-11-15 15:18:38 +0800

    update

--- nixos/hosts/cn2-la/default.nix
+++ nixos/hosts/cn2-la/default.nix
@@ -1,7 +1,13 @@
 { pkgs, lib, config, modules, ... }:
-with lib;{
-  imports =
-    with modules; [ base networking globals gravity ./disk-config.nix ./secrets.nix ];
+with lib; {
+  imports = with modules; [
+    base
+    networking
+    globals
+    gravity
+    ./disk-config.nix
+    ./secrets.nix
+  ];
 
   config = {
     system.stateVersion = "24.11";
@@ -37,21 +43,17 @@ with lib;{
         organization = "rmtt.tech";
         commonName = "cn2-la";
         privateKey = config.sops.secrets.ipsec-private.path;
-        endpoints = [
-          {
-            serialNumber = "0";
-            addressFamily = "ip4";
-            address = "cn2-la.rmtt.host";
-          }
-        ];
+        endpoints = [{
+          serialNumber = "0";
+          addressFamily = "ip4";
+          address = "cn2-la.rmtt.host";
+        }];
       };
       reload = {
         enable = true;
         headerFile = config.sops.secrets.header.path;
       };
-      strongswan = {
-        interfaces = [ "ens3" ];
-      };
+      strongswan = { interfaces = [ "ens3" ]; };
       address = [ "2a0c:b641:69c:5210::1/60" ];
       bird = {
         enable = true;
@@ -61,10 +63,26 @@ with lib;{
         enable = true;
         prefix = "2a0c:b641:69c:5210:8964::/96";
       };
-			srv6 = {
-				enable = true;
-				prefix = "2a0c:b641:69c:521";
-			};
+      srv6 = {
+        enable = true;
+        prefix = "2a0c:b641:69c:521";
+      };
+    };
+
+    systemd.network.netdevs.k3s = {
+      netdevConfig = {
+        Kind = "dummy";
+        Name = "k3s";
+      };
+    };
+
+    systemd.network.networks.k3s = {
+      matchConfig = { Name = "k3s"; };
+
+      networkConfig = {
+        Address = "192.168.128.32";
+        Description = "NIC for k3s";
+      };
     };
   };
 }
