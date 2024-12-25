dfcae9aafae997982c96063990b14d613cc02af0
RMT <d.rong@outlook.com>
2024-12-25 10:37:54 +0800

    update cider

--- packages/cider2.nix
+++ packages/cider2.nix
@@ -1,23 +1,20 @@
 { appimageTools, fetchurl }:
 
 let
-  pname = "cider";
-  version = "2.5.0";
+  pname = "Cider";
+  version = "2.6.0";
   name = "${pname}-${version}";
 
   src = fetchurl {
-    url = "https://cloud.rmtt.tech/s/qqzRgg5FN26BdEs/download";
-    sha256 = "sha256-HwfByY8av1AvI+t7wnaNbhDLXBxgzRKYiLG1hPUto9o=";
+    url = "https://cloud.rmtt.tech/s/c6XLCSq9gjR2TbG/download";
+    sha256 = "sha256-q9ulXYha5PSZbYZ/oxOvGvK5XGn0TlAGMymju5fXwmU=";
   };
-  appimageContents = appimageTools.extractType1 { inherit name src; };
-in appimageTools.wrapType1 {
-  inherit name src;
+  appimageContents = appimageTools.extract { inherit pname src version; };
+in appimageTools.wrapType2 {
+  inherit pname src version;
 
   extraInstallCommands = ''
-        mv $out/bin/${name} $out/bin/${pname}
         install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
-        substituteInPlace $out/share/applications/${pname}.desktop \
-          --replace-fail 'Exec=AppRun' 'Exec=${pname} ''${NIXOS_OZONE_WL:+''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime}}'
         cp -r ${appimageContents}/usr/share/icons $out/share
     		'';
 
